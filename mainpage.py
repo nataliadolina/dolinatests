d8214d906fd864a422e380cbe1a9131dac7c6557
Natalia <nat.dol30@mail.ru>
2019-03-24 00:03:12 +0300

    -- authorization and partly registration added

--- mainpage.py
+++ mainpage.py
@@ -12,14 +12,40 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    username = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Enter')
 
 
 base = DB()
 users_base = UsersModel(base)
+
+
+class RegistrateForm(FlaskForm):
+    username = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeatpassword = PasswordField('Repeat password', validators=[DataRequired()])
+    submit = SubmitField('Done')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegistrateForm()
+    if form.validate_on_submit():
+        f1, f2 = form.username.data, form.password.data
+        exists = users_base.exists(f1, f2)
+        if exists[0]:
+            form.username.data, form.password.data = '', ''
+            return render_template('registration.html',
+                                   text='пользователь с таким же именем уже существует. Смените логин', form=form)
+        else:
+            session['username'] = form.username.data
+            nm = UsersModel(base)
+            nm.insert(form.username.data, form.password.data)
+            session['user_id'] = users_base.exists(form.username.data, form.password.data)[1]
+            return redirect('/index')
+    return render_template('registration.html', text='', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
