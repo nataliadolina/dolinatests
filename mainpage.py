187fba8b4e0c636827a7b53930795abb8be22d95
Natalia <nat.dol30@mail.ru>
2019-03-23 14:38:22 +0300

    -- navbar

--- mainpage.py
+++ mainpage.py
@@ -10,6 +10,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    with open("json/logins_passwords", "rt", encoding="utf8") as f:
+        f = json.loads(f.read())
+    if form.validate_on_submit():
+        for i in range(len(f)):
+            if request.form['username'] in f[i]['login'] and request.form['password'] in f[i]['password']:
+                return redirect('/index')
+        return redirect('/failure')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
